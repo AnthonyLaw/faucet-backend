d9c9bac2133ffaa508048f8ac3a27c91c6df4c95
AnthonyLaw <yc-law1015@hotmail.com>
2022-02-23 07:11:25 +0800

    [backend] task: add twitter request route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const nemController = require('./controllers/nem');
+const twitterController = require('./controllers/twitter');
 const { nemFaucetValidation, toAbsoluteAmount, toRelativeAmount } = require('./utils/helper');
 const dotenv = require('dotenv');
 const restify = require('restify');
@@ -65,6 +66,55 @@ server.post('/claim/xem', async (req, res, next) => {
 
 // Todo
 // server.post('/claim/xym', (req, res, next) => {});
+
+server.get('/twitter/auth', async (req, res, next) => {
+	try {
+		const result = await twitterController.requestToken();
+
+		if ('true' === result.oauth_callback_confirmed) {
+			res.send(result);
+			return next(false);
+		}
+
+		return next(new restifyErrors.InternalError('Twitter auth failed.'));
+	} catch (error) {
+		return next(error);
+	}
+});
+
+server.get('/twitter/verify', async (req, res, next) => {
+	try {
+		const { client, ...info } = await twitterController.userAccess(req.params);
+
+		res.send(info);
+
+		return next(false);
+	} catch (error) {
+		return next(error);
+	}
+});
+
+server.get('/twitter/user', async (req, res, next) => {
+	try {
+		const { followers, userInfo } = await twitterController.getUserData(req.params);
+
+		const diff = Math.abs(new Date() - new Date(userInfo.created_at));
+		const accountAge = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+		const isVerify = 10 === followers.length && 30 < accountAge;
+
+		const result = {
+			isVerify,
+			screenName: userInfo.username
+		};
+
+		res.send(result);
+
+		return next(false);
+	} catch (error) {
+		return next(error);
+	}
+});
 
 server.listen(process.env.PORT, () => {
 	console.info('%s listening at %s', server.name, server.url);
